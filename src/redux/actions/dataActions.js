039803cd7775d8916aa9d61fd183222b422cbf84
olektymchenko <58951697+olektymchenko@users.noreply.github.com>
2020-05-11 22:00:23 +0200

    some good stuff added

--- src/redux/actions/dataActions.js
+++ src/redux/actions/dataActions.js
@@ -408,3 +408,15 @@ export const sendMessage = (messageId, message) => (dispatch) => {
         dispatch({ type: STOP_LOADING_MESSAGES })
     })
 }
+
+export const sendImage = (messageId, image) => (dispatch) => {
+    dispatch({ type: START_LOADING_MESSAGES });
+    axios.post(`/messages/addimage/${messageId}`, image).then(res => {
+        dispatch({ type: CLEAR_ERRORS })
+    }).then(res => {
+        dispatch({ type: STOP_LOADING_MESSAGES })
+    }).catch(err => {
+        console.log(err);
+        dispatch({ type: STOP_LOADING_MESSAGES })
+    })
+}
